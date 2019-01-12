7c5ea2fbe41d5be271cacab357499da7417d18cd
Jordan Huang <good5dog5@gmail.com>
2019-01-12 22:25:34 +0800

    Final change.

--- js/about.js
+++ js/about.js
@@ -1,7 +1,31 @@
 // JavaScript Document
 $(document).ready(function(){
-	$(".box").click(function(){
+	$("#box1").click(function(){
 		$(".content").fadeToggle(1000);
+		$("#content1").hide();
+		$("#content2").hide();
+		if($(this).hasClass("ch_bg")){
+			$(this).removeClass("ch_bg")
+		}else{
+			$(this).addClass("ch_bg");
+		}
+		
+	});
+	$("#box2").click(function(){
+		$(".content2").fadeToggle(1000);
+		$("#content").hide();
+		$("#content1").hide();
+		if($(this).hasClass("ch_bg")){
+			$(this).removeClass("ch_bg")
+		}else{
+			$(this).addClass("ch_bg");
+		}
+		
+	});
+	$("#box3").click(function(){
+		$(".content3").fadeToggle(1000);
+		$("#content").hide();
+		$("#content2").hide();
 		if($(this).hasClass("ch_bg")){
 			$(this).removeClass("ch_bg")
 		}else{
